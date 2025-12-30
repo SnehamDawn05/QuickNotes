3aa019876195931dff0dc44b937a87dd13985ff0
Sneham Dawn <dawnsneham@gmail.com>
2025-12-30 22:53:03 +0530

    building the NoteCards

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import noteRoutes from "./routers/noteRouters.js";
 import {connectDB} from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimitter from "./middleware/rateLimitter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,8 +11,10 @@ const app= express();
 const PORT=process.env.PORT;
 
 
-
 //middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+}))
 app.use(express.json()) //this middleware will parse the json(req.body)
 app.use(rateLimitter)
 
